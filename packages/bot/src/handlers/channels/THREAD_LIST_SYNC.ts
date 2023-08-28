6b014b3d9c92d4d562a3e7ce066fd3af85dd7194
livelove1987 <mateusz.luboinski@gmail.com>
2023-08-28 23:01:38 +0200

    fix: Added threadListSync event support

--- packages/bot/src/handlers/channels/THREAD_LIST_SYNC.ts
+++ packages/bot/src/handlers/channels/THREAD_LIST_SYNC.ts
@@ -6,7 +6,7 @@ export async function handleThreadListSync(bot: Bot, data: DiscordGatewayPayload
 
   const guildId = bot.transformers.snowflake(payload.guild_id)
 
-  bot.events.threadListSync?.({
+  await bot.events.threadListSync?.({
     guildId,
     channelIds: payload.channel_ids?.map((id) => bot.transformers.snowflake(id)),
     threads: payload.threads.map((thread) => bot.transformers.channel(bot, { channel: thread, guildId })),
